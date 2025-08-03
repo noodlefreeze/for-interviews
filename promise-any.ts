4f027bc0b3150cf286a22edebc99e81ae90bc3be
lewani <noodlefreeze@gmail.com>
2025-08-04 05:42:27 +0800

    promise any

---
+++ promise-any.ts
@@ -0,0 +1,33 @@
+export default function promiseAny<T>(iterable: Iterable<T>): Promise<T> {
+  return new Promise((resolve, reject) => {
+    const items = Array.from(iterable)
+    if (items.length === 0) {
+      return reject(new AggregateError([], 'All promises were rejected'))
+    }
+
+    let hasDone = false
+    const errors: unknown[] = new Array(items.length)
+    let caughtCount = 0
+
+    items.forEach((item, index) => {
+      Promise.resolve(item)
+        .then((value) => {
+          if (!hasDone) {
+            hasDone = true
+            resolve(value)
+          }
+        })
+        .catch((error) => {
+          if (hasDone) return
+
+          errors[index] = error
+          caughtCount++
+
+          if (caughtCount === items.length) {
+            hasDone = true
+            reject(new AggregateError(errors, 'All promises were rejected'))
+          }
+        })
+    })
+  })
+}
